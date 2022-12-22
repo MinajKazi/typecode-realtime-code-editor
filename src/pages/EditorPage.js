7513bc0000ab6d5914f8a0f028ef383cb6ac0b64
Minaj Kazi <minajkazi7777@gmail.com>
2022-12-22 23:44:59 +0530

    first commit

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -24,13 +24,11 @@ const EditorPage = () => {
             socketRef.current = await initSocket();
             socketRef.current.on('connect_error', (err) => handleErrors(err));
             socketRef.current.on('connect_failed', (err) => handleErrors(err));
-
             function handleErrors(e) {
                 console.log('socket error', e);
                 toast.error('Socket connection failed, try again later.');
                 reactNavigator('/');
             }
-
             socketRef.current.emit(ACTIONS.JOIN, {
                 roomId,
                 username: location.state?.username,
