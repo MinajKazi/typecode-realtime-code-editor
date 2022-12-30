9b83612fbfdf048fb0de8d12bcd269e6acdcedb0
Minaj Kazi <minajkazi7777@gmail.com>
2022-12-30 22:31:22 +0530

    env update

--- src/socket.js
+++ src/socket.js
@@ -8,5 +8,5 @@ export const initSocket = async () => {
         transports: ['websocket'],
     };
     //need to replace this process.env.REACT_APP_BACKEND_URL with "http://localhost:5000" to run on local machine
-    return io(process.env.REACT_APP_BACKEND_URL, options);
+    return io("http://localhost:5000", options);
 };
